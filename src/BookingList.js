4ef76c7b7c1bcb772700892f666d12297ffa3d5f
Aniket Acharya <aniketacharya30@gmail.com>
2025-07-13 11:18:25 +0530

    socket

--- src/BookingList.js
+++ src/BookingList.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("https://train-booking-backend-ajzh.onrender.com");
 function BookingList() {
   const [bookings, setBookings] = useState([]);
 
   useEffect(() => {
+    //fetch initial bookings
     fetch("https://train-booking-backend-ajzh.onrender.com/api/bookings")
       .then((res) => res.json())
       .then((data) => setBookings(data))
       .catch((err) => console.error("Error fetching bookings:", err));
+
+       socket.on("newBooking", (booking)=>{
+    setBookings((prev)=> [booking, ...prev]);
+  });
+  // Cleanup on unmount
+  return () => socket.off("newBooking");
   }, []);
+
+ 
+ 
 
   return(
     <div style={{ margin: "20px auto", maxWidth: "500px" }}>
@@ -21,8 +34,5 @@ function BookingList() {
       </ul>
     </div>
   );
-  
-
-
 }
 export default BookingList;
